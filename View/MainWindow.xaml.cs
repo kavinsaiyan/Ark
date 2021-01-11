91c512be35c820787d1502a154859c6698829f32
Captain Newt <42378068+NNeerly@users.noreply.github.com>
2021-01-12 03:44:21 +0800

    Initial commit

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace Ark
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,4 +23,5 @@ namespace Ark
             InitializeComponent();
         }
     }
+
 }
